7dd9840e3b7d60dcf161e9317d5102482e44e7c7
chanduhvg <chanduhvg@gmail.com>
2020-03-06 17:33:51 -0600

    added

---
+++ mathcalculation.js
@@ -0,0 +1,8 @@
+function multiplyBy() {
+    num1 = document.getElementById("Number1").value;
+    num2 = document.getElementById("Number2").value;
+    num3 = document.getElementById("Number3").value;
+    num4 = document.getElementById("Number4").value;
+    document.getElementById("Number5").value = num1 * num2 * num3 * num4;
+}
+    
