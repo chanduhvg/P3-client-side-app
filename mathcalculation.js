2a066f00619f1fe56f625cb2bef2ad0d0c09f3d0
chanduhvg <chanduhvg@gmail.com>
2020-03-06 19:37:59 -0600

    all files added

--- mathcalculation.js
+++ mathcalculation.js
@@ -3,6 +3,7 @@ function multiplyBy() {
     num2 = document.getElementById("Number2").value;
     num3 = document.getElementById("Number3").value;
     num4 = document.getElementById("Number4").value;
-    document.getElementById("Number5").value = num1 * num2 * num3 * num4;
+    document.getElementById("Number5").value = fn;
 }
     
+const fn=(x,y,z,a)=>{return x*y*z*a}
