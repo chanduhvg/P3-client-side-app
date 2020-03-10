c03f0fd9eb6f8f6b00dd812c3619bc0393fc0904
chanduhvg <chanduhvg@gmail.com>
2020-03-09 21:53:12 -0500

    added

--- mathcalculation.js
+++ mathcalculation.js
@@ -1,10 +1,79 @@
-function multiplyBy() {
-    num1 = document.getElementById("Number1").value;
-    num2 = document.getElementById("Number2").value;
-    num3 = document.getElementById("Number3").value;
-    num4 = document.getElementById("Number4").value;
-    document.getElementById("Number5").value = num1 * num2 * num3 * num4;
+function mult(a,b,c,d){
 
+    if(isNaN(a || b || c || d)){
+
+        throw error('The given argument is not number')
+    }
+
+   return a*b*c*d
 }
 
- 
+
+
+
+function multiplyBy()
+{
+      const  num1 = document.getElementById("firstNumber").value;
+      const  num2 = document.getElementById("secondNumber").value;
+      const  num3 = document.getElementById("thirdNumber").value;
+      const  num4 = document.getElementById("fourthNumber").value;
+      
+      document.getElementById("result").innerHTML = mult(num1,num2,num3,num4)
+}
+
+
+// $(document).ready(function(){
+//     $('#firstNumber').val(localStorage.getItem('firstNumber'));
+//     $('#secondNumber').val(localStorage.getItem('secondNumber'));
+//     $('#thirdNumber').val(localStorage.getItem('thirdNumber'));
+//     $('#fourthNumber').val(localStorage.getItem('fourthNumber'));
+    
+// })
+
+// const Multiply=(num1,num2,num3,num4)=>{return ((num1 * num2 * num3 * num4) / 100)}
+
+// function multiplyBy() {
+//     let firstNumber;
+//     let secondNumber;
+//     let thirdNumber;
+//     let fourthNumber;
+
+//     firstNumber = document.getElementById('firstNumber').value;
+//     localStorage.setItem('firstNumber', firstNumber);
+
+//     secondNumber = document.getElementById('secondNumber').value;
+//     localStorage.setItem('secondNumber', secondNumber);
+   
+//     thirdNumber = document.getElementById('thirdNumber').value;
+//     localStorage.setItem('thirdNumber', thirdNumber);
+   
+//     fourthNumber = document.getElementById('fourthNumber').value;
+//     localStorage.setItem('fourthNumber', fourthNumber);
+
+//     var result = Multiply(firstNumber,secondNumber,thirdNumber,fourthNumber);
+    
+//     document.getElementById('Multiply').value = result;
+// }
+
+const jokeURI = 'https://api.icndb.com/jokes/random?limitTo=[nerdy]'
+
+// fetch information
+const getJoke = async () => {
+  try {
+    const response = await fetch(jokeURI)
+    const obj = await response.json()
+    console.log(`FETCHED. Response JSON ${obj}`)
+    const joke = obj.value.joke || 'No joke for you.'
+    return joke
+  } catch (error) { console.error(error) }
+}
+
+// interact with DOM
+const updateWithJoke = async (event) => {
+  try {
+    document.querySelector('#result').innerHTML = ''
+    const answer = await getJoke()
+    document.querySelector('#result').innerHTML = answer
+  } catch (error) { console.error(error) }
+}
+
--- mathcalculation.test.js
+++ mathcalculation.test.js
@@ -1,35 +1,37 @@
 QUnit.module('MAIN MODULE', {})  // group all these tests together
 
 QUnit.test('TEST multiplication', assert => {
-  assert.equal(fn(2,2,2,2), 16, 'multiplication value')
-  assert.equal(fn(-2,2,2,2), -16, 'Negative multiplication value')
-  assert.equal(fn(0,8,2,2), 0, 'Zero multiplication value')
-  assert.equal(fn(2,2,2,2.2), 17.6, 'decimal multiplication value')
-  assert.equal(fn(0,0,0,0), 0, 'zero multication value')
+  assert.equal(mult(2,2,2,2), 16, 'multiplication value')
+  assert.equal(mult(-2,2,2,2), -16, 'Negative multiplication value')
+  assert.equal(mult(0,8,2,2), 0, 'Zero multiplication value')
+  assert.equal(mult(2,2,2,2.2), 17.6, 'decimal multiplication value')
+  assert.equal(mult(0,0,0,0), 0, 'zero multication value')
 })
 
-QUnit.config.autostart = false 
+// QUnit.config.autostart = false 
 
 
-window.addEventListener('load', () => {
-    const appURL = '../index.html' // reach out to the html for the app (js-gui)
-    const openingTag = '<main class="container mt-5 flex-fill">'
-    const closingTag = '</main>' // go grab it!
-    fetch(appURL, { method: 'GET' })
-      .then(response => {
-        return response.text() // returns promise
-      })
-      .then(txt => {                
-        const start = txt.indexOf(openingTag)
-        const end = txt.indexOf(closingTag) + closingTag.length
-        const html = txt.substring(start, end) // we only want part of the page
-        const qunitFixtureBody = document.querySelector('#qunit-fixture')
-        qunitFixtureBody.innerHTML = html // put the page into the DOM - the second div associated with this page
-        console.info(qunitFixtureBody) // print it out so we can see it (it doesn't get inserted into the page)
-        QUnit.start() // start the actual testing - it finds and runs both the tests, defined in QUnit.test()
-      })
-      .catch(error => {
-        console.error('error:', error);
-        QUnit.start()
-      })
-  })
+// window.addEventListener('load', () => {
+//     const appURL = '../index.html' // reach out to the html for the app (js-gui)
+//     const openingTag = '<main class="container mt-5 flex-fill">'
+//     const closingTag = '</main>' // go grab it!
+//     fetch(appURL, { method: 'GET' })
+//       .then(response => {
+//         return response.text() // returns promise
+//       })
+//       .then(txt => {                
+//         const start = txt.indexOf(openingTag)
+//         const end = txt.indexOf(closingTag) + closingTag.length
+//         const html = txt.substring(start, end) // we only want part of the page
+//         const qunitFixtureBody = document.querySelector('#qunit-fixture')
+//         qunitFixtureBody.innerHTML = html // put the page into the DOM - the second div associated with this page
+//         console.info(qunitFixtureBody) // print it out so we can see it (it doesn't get inserted into the page)
+//         QUnit.start() // start the actual testing - it finds and runs both the tests, defined in QUnit.test()
+//       })
+//       .catch(error => {
+//         console.error('error:', error);
+//         QUnit.start()
+//       })
+
+    
+  
