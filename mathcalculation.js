ceaaad87bfef88ddfebccaca8c4e8f41ef623df3
chanduhvg <chanduhvg@gmail.com>
2020-03-17 15:06:54 -0500

    added ajax

--- mathcalculation.js
+++ mathcalculation.js
@@ -8,9 +8,6 @@ function mult(a,b,c,d){
    return a*b*c*d
 }
 
-
-
-
 function multiplyBy()
 {
       const  num1 = document.getElementById("firstNumber").value;
@@ -27,38 +24,29 @@ function multiplyBy()
       let d=localStorage.getItem("num4")
       let e=mult(a,b,c,d)
       document.getElementById("result").innerHTML = e
+    }
+    const jokeURI = 'https://dog.ceo/api/breeds/image/random'
+
+// fetch information
+const getJoke = async () => {
+    try {
+        const response = await fetch(jokeURI)
+        const obj = await response.json()
+        console.log(`FETCHED. Response JSON ${obj}`)
+        const joke = obj.message || 'No joke for you.'
+        return joke
+    } catch (error) { console.error(error) }
 }
+const updateWithJoke = async (event) => {
+    try {
+        document.querySelector('#dogarea').src = ''
+        const answer = await getJoke()
+        document.querySelector('#dogarea').src = answer
+    } catch (error) { console.error(error) }
+}
+// document.getElementById('getscore').onclick(getJoke);
+document.addEventListener('click', event => {
+    if (event.target && event.target.id === 'getscore') { updateWithJoke(event) }
+})
 
 
-// $(document).ready(function(){
-//     $('#firstNumber').val(localStorage.getItem('firstNumber'));
-//     $('#secondNumber').val(localStorage.getItem('secondNumber'));
-//     $('#thirdNumber').val(localStorage.getItem('thirdNumber'));
-//     $('#fourthNumber').val(localStorage.getItem('fourthNumber'));
-    
-// })
-
-// const Multiply=(num1,num2,num3,num4)=>{return ((num1 * num2 * num3 * num4) / 100)}
-
-// function multiplyBy() {
-//     let firstNumber;
-//     let secondNumber;
-//     let thirdNumber;
-//     let fourthNumber;
-
-//     firstNumber = document.getElementById('firstNumber').value;
-//     localStorage.setItem('firstNumber', firstNumber);
-
-//     secondNumber = document.getElementById('secondNumber').value;
-//     localStorage.setItem('secondNumber', secondNumber);
-   
-//     thirdNumber = document.getElementById('thirdNumber').value;
-//     localStorage.setItem('thirdNumber', thirdNumber);
-   
-//     fourthNumber = document.getElementById('fourthNumber').value;
-//     localStorage.setItem('fourthNumber', fourthNumber);
-
-//     var result = Multiply(firstNumber,secondNumber,thirdNumber,fourthNumber);
-    
-//     document.getElementById('Multiply').value = result;
-// }
